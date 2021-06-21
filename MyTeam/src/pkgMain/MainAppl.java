33e0bc5b0d3df4a7c45fb332adcc8aa552b24c01
TscharnigGmBH <43322224+TscharnigGmBH@users.noreply.github.com>
2021-06-21 14:24:51 +0200

    Update MainAppl.java

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -5,8 +5,6 @@ import pkgData.Player;
 public class MainAppl {
 
 	public static void main(String[] args) {
-		Player player = new Player(2, "Dragovic", "Defense");		
-		System.out.println("== " + player);
 		
 		Player player2 = new Player(4, "Hinteregger", "Defense");		
 		System.out.println("== " + player2);
